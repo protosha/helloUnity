7e7d2ac1e8fff0d07171e79c2e1c6378a6d63f4c
Max Tsoy <protosha123@gmail.com>
2017-07-26 22:31:23 +0300

    Added jumping and shooting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,27 +15,47 @@ public class MoveDirectionEventArgs : EventArgs {
 
 public class PlayerController : MonoBehaviour {
 
-    [SerializeField]
-    private float maxWalkSpeed = 10f;
+    [SerializeField] private float maxWalkSpeed = 10f;
+
+	// Jump fields
+	[SerializeField] private Transform groundChecker;
+	[SerializeField] private LayerMask groundLayer;
+	[SerializeField] private float jumpHeight = 100f;
+	private float groundCheckerRadius = 0.2f;
+	private bool isOnGround = true;
+
+	// Shooting fields
+	[SerializeField] private Transform gunTip;
+	[SerializeField] private GameObject projectile;
+	[SerializeField] private float shootRate = 0.5f;
+	private bool isShooting = false;
+	private float lastShot = 0.0f;
+
 
     private Rigidbody2D charRB;
     private Animator animator;
-    private Direction moveDirection = Direction.Right;
+    private Direction moveDirection;
+
+
 
     public delegate void MoveDirectionChangedHandler(MoveDirectionEventArgs e);
     public event MoveDirectionChangedHandler MoveDirectionChanged;
 
     public Direction MoveDirection {
-        get {
-            return this.moveDirection; 
-        }
-        set {
-            this.moveDirection = value;
+		get {
+			return this.moveDirection;
+		}
+		set {
+			// Update direction and call event only with new value
+			if (this.moveDirection != value) {
+				this.moveDirection = value;
 
-            var eventArgs = new MoveDirectionEventArgs(this.moveDirection);
-            this.MoveDirectionChanged(eventArgs);
-        }
-    }
+				// Direction changed - call event
+				var eventArgs = new MoveDirectionEventArgs(this.moveDirection);
+				this.MoveDirectionChanged(eventArgs);
+			}
+		}
+	}
 
 
 	// Use this for initialization
@@ -43,23 +63,59 @@ public class PlayerController : MonoBehaviour {
         this.charRB = this.GetComponent<Rigidbody2D>();
         this.animator = this.GetComponent<Animator>();
         this.MoveDirectionChanged += OnMoveDirectionChanged;
+
+        // Set direction on start
+        this.MoveDirection = Direction.Right;
+	}
+
+	// Update is called every frame
+	void Update() {
+		// Set all animation dependencies
+		this.animator.SetBool("isOnGround", this.isOnGround);
+		this.animator.SetFloat("xSpeed", Mathf.Abs(this.charRB.velocity.x));
+		this.animator.SetFloat("ySpeed", this.charRB.velocity.y);
+		this.animator.SetBool("isShooting", this.isShooting);
 	}
 	
 	// Update is called every fixed period of time
 	void FixedUpdate () {
+        // Get inputs and calculate speed
         float horizAxis = Input.GetAxis("Horizontal");
         float walkSpeed = horizAxis * this.maxWalkSpeed;
 
-        //charRB.AddForce(new Vector2(horizAxis * this.maxWalkSpeed, 0));
-        charRB.velocity = new Vector2(walkSpeed, charRB.velocity.y);
+        // Movement
+        this.charRB.velocity = new Vector2(walkSpeed, this.charRB.velocity.y);
 
-        animator.SetFloat("speed", Mathf.Abs(walkSpeed));
-
+        // Set move direction (all direction logic is performed in the property event)
         if (walkSpeed > 0) {
             this.MoveDirection = Direction.Right;
         } else if (walkSpeed < 0) {
             this.MoveDirection = Direction.Left;
         }
+
+		// Jumps
+		bool isJumpPressed = Input.GetAxis("Jump") > 0;
+		if (this.isOnGround && isJumpPressed) {
+			this.isOnGround = false;
+			this.charRB.AddForce(new Vector2(0, this.jumpHeight));
+		} else {
+			this.isOnGround = Physics2D.OverlapCircle(this.groundChecker.position, this.groundCheckerRadius, this.groundLayer);
+		}
+
+		// Depending on the input, provide shooting animation dependencies
+		bool isFirePressed = Input.GetAxisRaw("Fire1") > 0;
+		bool isFirstShot = false;
+		if (isFirePressed && !this.isShooting) { // Apply animation only once
+			this.isShooting = true;
+			isFirstShot = true;
+		} else if (!isFirePressed) {
+			this.isShooting = false;
+		}
+
+		// Shooting
+		if (this.isShooting) {
+			this.Shoot(isFirstShot);
+		}
 	}
 
     private void OnMoveDirectionChanged(MoveDirectionEventArgs e) {
@@ -71,4 +127,12 @@ public class PlayerController : MonoBehaviour {
         }
         this.transform.localScale = charScale;
     }
+
+	private void Shoot(bool isFirstShot) {
+		if (Time.time > this.lastShot || isFirstShot) {
+			this.lastShot = Time.time + this.shootRate;
+			GameObject projectile = Instantiate(this.projectile, this.gunTip.position, Quaternion.identity);
+			projectile.SendMessage("OnProjectileSpawned", this.MoveDirection);
+		}
+	}
 }
---
+++ Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour {
+
+	[SerializeField] private float flySpeed = 2.0f;
+	[SerializeField] private float lifespan = 2.0f;
+
+	private Rigidbody2D projRB;
+
+	void Awake() {
+		projRB = this.GetComponent<Rigidbody2D>();
+	}
+
+	// Use this for initialization
+	void Start() {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnProjectileSpawned(Direction direction) {
+		Debug.Log("Inside spawn method");
+		if (direction == Direction.Right) {
+			this.projRB.AddForce(Vector2.right * flySpeed);
+		} else if (direction == Direction.Left) {
+			this.transform.rotation = Quaternion.Euler(0, 0, 180f);
+			this.projRB.AddForce(Vector2.left * flySpeed);
+		}
+
+		Destroy(this.gameObject, lifespan);
+	}
+}
